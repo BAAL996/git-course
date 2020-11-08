9504399beab3b60ae3c33646c6764f834e4e2d87
Baal996 <vladislav.stepanenko996@gmail.com>
2020-11-08 16:24:15 +1000

    hello

--- one.js
+++ one.js
@@ -3,3 +3,8 @@ let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 for (let a in arrayNumbers){
     console.log(a);
 }
+
+const hello = (number) => {
+    console.log(number);
+    console.log(number**number)
+}
