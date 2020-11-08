6e97faff99e3802ecffbffd49967e8f222a989cd
Baal996 <vladislav.stepanenko996@gmail.com>
2020-11-08 16:03:00 +1000

    one

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ console.log('Hello World!')
 let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8]
 for (let a in arrayNumbers){
     console.log(a);
+
 }
+
+const sum = (arr) =>{
+    let sum = 0
+    for (let a in arr){
+        sum += a;
+    }
+    return sum;
+}
+console.log(sum(arrayNumbers))
---
+++ one.js
@@ -0,0 +1,5 @@
+console.log('Hello world!');
+let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+for (let a in arrayNumbers){
+    console.log(a);
+}
